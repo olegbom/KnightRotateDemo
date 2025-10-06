722edf97185f81722519829bd30ac1c9aff873db
Oleg Vysotsky <olegbom@gmail.com>
2025-10-06 18:02:02 +0300

    First Commit

---
+++ Field.cs
@@ -0,0 +1,32 @@
+using Raylib_cs;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace KnightRotateDemo;
+
+public class Field
+{
+    public int Size {get; init;} = 10;
+
+    public int CellSize{get; init;} = 40;
+
+    public Field()
+    {
+        
+    }
+
+    public void GridDraw()
+    {
+        for (int i = 0; i < Size + 1; i++)
+        {
+            int t = CellSize * (i) + 1;
+            int max = CellSize * Size + 1;
+            Raylib.DrawLine(0, t, max, t, Color.Red);
+            Raylib.DrawLine(t, 0, t, max, Color.Red);
+            for (int j = 0; j < 10; j++)
+            {
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Raylib_cs;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace KnightRotateDemo;
+
+internal static class Program
+{
+    // STAThread is required if you deploy using NativeAOT on Windows - See https://github.com/raylib-cs/raylib-cs/issues/301
+    [System.STAThread]
+    public static void Main()
+    {
+        Raylib.InitWindow(800, 480, "Hello World");
+        Field f = new Field();
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+
+            f.GridDraw();
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
