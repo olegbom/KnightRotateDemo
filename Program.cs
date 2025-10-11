deb1bf2efd45df037c0b19f08f59106c4f8e3596
olegbom <olegbom@inbox.ru>
2025-10-11 08:49:39 +0300

    start make wasm

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿using Raylib_cs;
+using System.Runtime.InteropServices.JavaScript;
 
 namespace KnightRotateDemo;
 
 internal static class Program
 {
     // STAThread is required if you deploy using NativeAOT on Windows - See https://github.com/raylib-cs/raylib-cs/issues/301
-    [System.STAThread]
+    // [System.STAThread]
     public static void Main()
     {
         Raylib.InitWindow(800, 480, "Hello World");
